67ace9734450c441890bcbda0a8c27e80b0ffd74
erick <e.kanja@alustudent.com>
2025-07-21 17:13:32 +0200

    Add solution to How many subs?

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Documentation to check how many subs for Reddit API
+"""
+import json
+import requests
+
+def number_of_subscribers(subreddit):
+    headers = {"User-Agent": "desktop:APIactivity:v1.0.0"}
+    query = f"https://www.reddit.com/r/{subreddit}/about.json"
+    res = requests.get(query, headers=headers)
+    return res.json()["data"]["subscribers"]
+
+if __name__ == "__main__":
+    number_of_subscribers("python")
