26d687e29216a5a703acdfcfacea01de58c795fd
erick <e.kanja@alustudent.com>
2025-07-21 17:32:07 +0200

    Add functionality to 0-subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,7 +8,10 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "desktop:APIactivity:v1.0.0"}
     query = f"https://www.reddit.com/r/{subreddit}/about.json"
     res = requests.get(query, headers=headers)
-    return res.json()["data"]["subscribers"]
+    if (res.status_code == 200):
+        return res.json()["data"]["subscribers"]
+    else:
+        return 0
 
 if __name__ == "__main__":
     number_of_subscribers("python")
